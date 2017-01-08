fc847f634d757a620a9f07fd98981ae40da8a480
Peter Hogan <peterjameshogan@gmail.com>
2017-01-08 21:54:55 +0000

    spark additions

--- newsreader/spark1.py
+++ newsreader/spark1.py
@@ -4,33 +4,85 @@ from pyspark import SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import *
 import argparse
+from nltk import word_tokenize, Text, collocations
 
-sc = SparkContext(appName='testing-python')
-scc = StreamingContext(sc, 2)
-sc.setLogLevel("ERROR")
-
+######### CLI arguments #########
 parser = argparse.ArgumentParser()
 parser.add_argument("broker")
 parser.add_argument("topic")
+parser.add_argument("checkpoint")
 
 args = parser.parse_args()
 
+######### define functions ########
+
+## counting function
+def updateFunction(newValues, runningCount):
+    if runningCount is None:
+        runningCount = 0
+    return sum(newValues, runningCount)
+
+def functionToCreateContext():
+    scc = StreamingContext(sc, 2)
+    sc.setLogLevel("ERROR")
+    scc.checkpoint(args.checkpoint)
+    return scc
+
+def filterUselessWords(word, blacklist):
+    if word in blacklist:
+        return False
+    return True
+
+def collocateWords(text):
+    tokens = word_tokenize(text)
+    return Text(tokens).collocations()
+
+wordBlacklist = ['', ' ', 'a', 'the', 'is', 'in', 'A', 'are','of','when','that','for','on', '|','as','to','The','an','and','News','-','with','at','it']
+
+sc = SparkContext(appName='testing-python')
+
 initialStateRDD = sc.parallelize([(u'test-data1', 1), (u'test-data2', 1)])
 
-kvs = KafkaUtils.createDirectStream(scc, [args.topic], {"metadata.broker.list": args.broker})
+context = StreamingContext.getOrCreate(args.checkpoint, functionToCreateContext)
+
+kvs = KafkaUtils.createDirectStream(context, [args.topic], {"metadata.broker.list": args.broker})
+
 lines = kvs.map(lambda x: x[1])
 
-#lines.pprint(num=40)
+co_words = lines.transform(lambda rdd: rdd.reduce(lambda txt: collocateWords(txt)))\
+                .pprint()
 
+'''
 counts = lines.flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word,1)) \
-        .reduceByKey(lambda a, b: a+b)\
-        .sortByKey(lambda _,a: a)
+              .filter(lambda a: filterUselessWords(a,wordBlacklist)) \
+              .map(lambda word: (word,1)) \
+              .updateStateByKey(updateFunction, initialRDD=initialStateRDD)
 
-windowed = counts.window(60,slideDuration=30)
+samplewords = counts.transform(lambda rdd: rdd.sample(withReplacement=False,fraction=0.5))
+sortwords = counts.transform(lambda rdd: rdd.sortBy(lambda a: a[1], ascending=False))
+'''
+
+context.start()
+context.awaitTermination()
+
+'''
+def functionToCreateContext():
+    sc = SparkContext(appName='testing-python')
+    scc = StreamingContext(sc, 2)
+    sc.setLogLevel("ERROR")
+    scc.checkpoint(args.checkpoint)
+    return scc
+
+              .reduceByKey(lambda a, b: a+b)\
+        .updateStateByKey(lambda a: a[1])
+
+counts.pprint()
+
+#windowed = counts.window(60,slideDuration=30)
 
 output = windowed.foreachRDD(counts.sortByKey(lambda b: b))
 output.pprint()
+'''
 
 #windowed = counts.window(60,slideDuration=30).pprint(num=50)
 #sortWindow = windowed.transform(sortByKey(lambda a, b: b))
@@ -38,6 +90,3 @@ output.pprint()
 
 #counts.saveAsTextFiles("test",suffix="txt")
 #counts.pprint(num=30)
-
-scc.start()
-scc.awaitTermination()
