94aacdca60a6447fe673d1ff2de8db01ffbc86ec
peterhogan <peterhogan@users.noreply.github.com>
2017-01-16 16:02:23 +0000

    JSON format added

--- webUI/webpage/demo/models.py
+++ webUI/webpage/demo/models.py
@@ -1,10 +1,14 @@
 from django.db import models
-#PERSON","LOCATION","MISC","ORGANIZATION","DATE
+
 class Person(models.Model):
+    name = models.CharField(max_length=200)
 
-
+class Location(models.Model):
+    
 
 class Organization(models.Model):
+
+class Misc(models.Model):
 
 class Date(models.Model):
 
