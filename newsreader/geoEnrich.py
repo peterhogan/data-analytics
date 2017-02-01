bb35b38c98f909606633d9ed4f687fc2b0ac03a7
Peter Hogan <peterjameshogan@gmail.com>
2017-02-01 22:28:16 +0000

    untested SQL query

--- newsreader/geoEnrich.py
+++ newsreader/geoEnrich.py
@@ -12,9 +12,11 @@ parser = argparse.ArgumentParser(description="query gazzeteer for geo data")
 parser.add_argument("query", help="string input to query DB with")
 parser.add_argument("-q", "--quiet", help="only output on error",action="store_true")
 parser.add_argument("-v", "--verbose", help="log level DEBUG",action="store_true")
+parser.add_argument("-i", "--input", help="input file to query",
+        action="store", required=True)
 parser.add_argument("-s", "--server", help="server address for geo queries",
         action="store", required=False, default="localhost")
-parser.add_argument("-o", "--output", help="output the relationships to a JSON file",
+parser.add_argument("-o", "--output", help="output a file",
                                     action="store", required=False)
 
 arg = parser.parse_args()
@@ -30,19 +32,42 @@ log = logging.getLogger('relaLog')
 
 ##############################
 
-geo_points = [["London",51,0]]
+# init the points list for html - for jscript list 
+geo_points = []
 
+# open the JSON file
+with open(arg.input, 'r') as inputfile:
+    nodedata = json.load(inputfile)
+
+# connect to the MySQL DB
 connect = mdb.connect(arg.server, 'peter', 'peter1', 'geonames');
 
-with connect:
-    cursor  = connect.cursor()
-    cursor.execute('''SELECT population, geonameid, name, countrycode, latitude, longitude 
-                      FROM geoname 
-                      WHERE name = %s 
-                      AND featureclass = "P"''', (arg.query,))
-    return_value = cursor.fetchall()
+### Don't know if I can keep the connect open for all queries or if I'll have to reopen it for each coord request
 
-coord_pair = [float(max(return_value)[4]),float(max(return_value)[5])]
+with connect:
+
+    # This might need to move inside the second for (or the first)
+    cursor  = connect.cursor()
+
+    for ind in range(len(nodedata)):
+        title = nodedata[ind]['title']
+        try:
+            for element in [nodedata[ind]['LOCATIONS'][j] for j in [i for i in nodedata[ind]['LOCATIONS']]]:
+                try:
+                    cursor.execute('''SELECT population, geonameid, name, countrycode, latitude, longitude 
+                                      FROM geoname 
+                                      WHERE name = %s''', (element,))
+                    return_value = cursor.fetchall()
+
+                    coord_tuple = [title,float(max(return_value)[4]),float(max(return_value)[5])]
+                    geo_points.append(coord_tuple)
+                    
+                except KeyError: # NOT THE RIGHT ERROR - will have to test the error thrown when you don't get a hit (try searching for gibberish
+                    pass
+
+
+        except KeyError:
+            pass
 
 
 #print("Time taken:", time() - timestart)
@@ -57,9 +82,9 @@ print('''
 <div id="mapid"></div>
 <script>
 
-var points = %(points)
+var points = %(points)r
 
-var mymap = L.map('mapid').setView(%(coord)r, 10);
+var mymap = L.map('mapid').setView([51,0], 2);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -73,7 +98,6 @@ for (var i = 0; i < points.length; i++) {
         .bindPopup(points[i][0])
         .addTo(mymap);
 }
-
 
 var popup = L.popup();
 
@@ -89,4 +113,4 @@ mymap.on('click', onMapClick);
 </script>
 <html>
 </html>
-''' % {'coord': coord_pair, 'query': arg.query, 'points': geo_points})
+''' % {'points': geo_points})
