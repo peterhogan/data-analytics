f72605703281b473d6ac04fef74b46e68442a89b
peterhogan <peterhogan@users.noreply.github.com>
2017-02-02 12:13:59 +0000

    working on news data

--- newsreader/geoEnrich.py
+++ newsreader/geoEnrich.py
@@ -1,3 +1,4 @@
+import random
 import MySQLdb as mdb
 import sys
 from time import time
@@ -9,9 +10,11 @@ timestart = time()
 
 ##### COMMAND LINE ARGUMENTS #####
 parser = argparse.ArgumentParser(description="query gazzeteer for geo data")
-parser.add_argument("query", help="string input to query DB with")
+#parser.add_argument("query", help="string input to query DB with")
 parser.add_argument("-q", "--quiet", help="only output on error",action="store_true")
 parser.add_argument("-v", "--verbose", help="log level DEBUG",action="store_true")
+parser.add_argument("-n", "--number", help="only process n entries",
+        action="store", required=False)
 parser.add_argument("-i", "--input", help="input file to query",
         action="store", required=True)
 parser.add_argument("-s", "--server", help="server address for geo queries",
@@ -35,42 +38,82 @@ log = logging.getLogger('relaLog')
 # init the points list for html - for jscript list 
 geo_points = []
 
+# cache queries for speed up
+locations = {}
+
+# init the counter (n)
+counter = 0
+
+# init jitter for dups
+#jitter = random.choice([1,-1])*random.random()/10000
+
 # open the JSON file
+log.debug("Opening file: %r", arg.input)
 with open(arg.input, 'r') as inputfile:
     nodedata = json.load(inputfile)
+    log.debug("File opened successfully")
 
 # connect to the MySQL DB
 connect = mdb.connect(arg.server, 'peter', 'peter1', 'geonames');
-
-### Don't know if I can keep the connect open for all queries or if I'll have to reopen it for each coord request
+log.debug("Connected to MySQL successfully")
 
 with connect:
 
-    # This might need to move inside the second for (or the first)
+    # initialise SQL cursor
     cursor  = connect.cursor()
 
     for ind in range(len(nodedata)):
-        title = nodedata[ind]['title']
+        roottitle = nodedata[ind]['title']
         try:
             for element in [nodedata[ind]['LOCATIONS'][j] for j in [i for i in nodedata[ind]['LOCATIONS']]]:
-                try:
-                    cursor.execute('''SELECT population, geonameid, name, countrycode, latitude, longitude 
-                                      FROM geoname 
-                                      WHERE name = %s''', (element,))
-                    return_value = cursor.fetchall()
+                title = roottitle+" : "+element
+                log.debug("Title is: %s", title)
+                if element in locations:
+                    log.debug("element %s found in locations dictionary", element)
+                    coord_tuple = [title, locations[element][0],locations[element][1]]
+                else:
+                    log.debug("element %s not found in dictionary, querying MySQL and adding", element)
+                    try:
+                        cursor.execute('''SELECT population, geonameid, name, countrycode, latitude, longitude 
+                                          FROM geoname 
+                                          WHERE name = %s''', (element,))
+                        return_value = cursor.fetchall()
+                        log.debug("Return value from query: %r",return_value)
 
-                    coord_tuple = [title,float(max(return_value)[4]),float(max(return_value)[5])]
-                    geo_points.append(coord_tuple)
-                    
-                except KeyError: # NOT THE RIGHT ERROR - will have to test the error thrown when you don't get a hit (try searching for gibberish
-                    pass
+                        coord_pair = [float(max(return_value)[4]),float(max(return_value)[5])]
+
+                        # add to dict to speed up queries
+                        log.debug("adding %r to locations dict", coord_pair)
+                        locations[element] = coord_pair
+
+                        coord_tuple = [title,coord_pair[0],coord_pair[1]]
+                        
+                    except ValueError:
+                        log.warn("Problem with database query - look at MySQL logs")
+                        pass
+
+                ## construct the coord tuple
+                if coord_tuple in geo_points:
+                    log.warn("duplicate point found: %r", coord_tuple)
+                    log.debug("applying jitter")
+                    coord_tuple = [coord_tuple[0],
+                                   random.choice([1,-1])*random.random()/100+coord_tuple[1],
+                                   random.choice([1,-1])*random.random()/100+coord_tuple[2]]
+                geo_points.append(coord_tuple)
+                log.debug("Tuple Value: %r", coord_tuple)
 
 
         except KeyError:
             pass
 
+        log.debug("Counter value: %i",counter)
+        counter += 1
 
-#print("Time taken:", time() - timestart)
+        if arg.number and counter >= int(arg.number):
+            break
+
+finishtime = time() - timestart
+log.debug("Time taken: %r", finishtime)
 print('''
 <!DOCTYPE html>
 <style>
